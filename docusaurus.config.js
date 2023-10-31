56c17e313f32f68a3a8162b405b9d05878037ae3
Kemil Beltre <kemil.beltre@interacso.com>
2023-10-31 13:40:53 +0100

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,130 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'OSJ Template',
+  tagline: 'I 💛 Open Source',
+  favicon: 'img/favicon.svg',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-test-site.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/opensourcejam/tree/main/documentation-template',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'OSJ Template',
+        logo: {
+          alt: 'OSJ Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'docSidebar',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            href: 'https://github.com/opensourcejam',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        logo: {
+          alt: 'Open Source JAM Logo',
+          src: 'img/logo_footer.svg',
+          href: 'https://opensourcejam.com',
+          width: 160,
+          height: 51,
+        },
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Get started',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.com/invite/8pRRswC7qH',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/opensourcejam_',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/opensourcejam',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Open Source Jam`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+}
+
+module.exports = config
---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import clsx from 'clsx'
+import styles from './styles.module.css'
+
+type FeatureItem = {
+  title: string
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>
+  description: JSX.Element
+}
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Feature 1',
+    Svg: require('@site/static/img/team_meeting_flash.svg').default,
+    description: <>Description</>,
+  },
+  {
+    title: 'Feature 2',
+    Svg: require('@site/static/img/programming_hold_code.svg').default,
+    description: <>Description </>,
+  },
+  {
+    title: 'Feature 3',
+    Svg: require('@site/static/img/business_deal_cash.svg').default,
+    description: <>Description </>,
+  },
+]
+
+function Feature({ title, Svg, description }: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
